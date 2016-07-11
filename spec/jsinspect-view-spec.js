5c7d6bb0b2dd7559276a54fa21315cae403f191f
Declan <declan@tinfish-creative.co.uk>
2016-07-11 08:57:07 +0100

    Initial Commit

---
+++ spec/jsinspect-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import JsinspectView from '../lib/jsinspect-view';
+
+describe('JsinspectView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
