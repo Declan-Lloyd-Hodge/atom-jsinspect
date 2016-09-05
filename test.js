4c2b9b11453513d01908784176a45688823f195f
Declan <declan@tinfish-creative.co.uk>
2016-09-05 10:37:54 +0100

    Just about functional

---
+++ test.js
@@ -0,0 +1,87 @@
+for(var i = 0;i < 50;i++){
+  if(true === true){
+    console.log("Hello \n World!");
+  }
+}
+
+for(var i = 0;i < 51;i++){
+  if(true === true){
+    console.log("Hello \n World!");
+  }
+}
+
+for(var i = 0;i < 52;i++){
+  if(true === true){
+    console.log("Hello \n World!");
+  }
+}
+
+for(var i = 0;i < 53;i++){
+  if(true === true){
+    console.log("Hello \n World!");
+  }
+}
+
+for(var i = 0;i < 54;i++){
+  if(true === true){
+    console.log("Hellp \n World!");
+  }
+}
+
+for(var i = 0;i < 55;i++){
+  if(true === true){
+    console.log("Hello \n World!");
+  }
+}
+
+for(var i = 0;i < 56;i++){
+  if(true === true){
+    console.log("Hello \n World!");
+  }
+}
+
+function test1(){
+  if(1 === 1 && 0 === 0 && true === true && false === false){
+    console.log("1");
+    console.log("2");
+    console.log("3");
+    console.log("4");
+    console.log("5");
+  }}
+
+function test2(){
+  if(1 === 1 && 0 === 0 && true === true && false === false){
+    console.log("1");
+    console.log("2");
+    console.log("3");
+    console.log("4");
+    console.log("5");
+  }}
+
+function test3(){
+  if(1 === 1 && 0 === 0 && true === true && false === false){
+    console.log("1");
+    console.log("2");
+    console.log("3");
+    console.log("4");
+    console.log("5");
+  }}
+
+function test4(){
+  if(1 === 1 && 0 === 0 && true === true && false === false){
+    console.log("1");
+    console.log("2");
+    console.log("3");
+    console.log("4");
+    console.log("5");
+  }}
+
+function test5(){
+  if(1 === 1 && 0 === 0 && true === true && false === false){
+    console.log("1");
+    console.log("2");
+    console.log("3");
+    console.log("4");
+    console.log("5");
+  }
+}
